7272762275fb220de50437e3c528b561bd1ee24e
sethvelasquez <seth.velasquez8908@stu.mtec.edu>
2023-05-12 19:13:28 -0600

    made changes to website

---
+++ Week5Day4finished/app.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    $('.navItem').css('color', 'white');
+    $('#nav').css('background-color', '#145');
+    $('li:even').css('background-color', '#ccc');
+    $('li:odd').css('background-color', '#aaa');
+    $('input').val("Seth")
+  });
